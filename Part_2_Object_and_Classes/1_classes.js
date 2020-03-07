860a67a060c2d7f73b30e2d28fea908da7052307
Stefano Balietti <futur.dorko@gmail.com>
2020-03-07 17:18:22 +0100

    more on classes and objects

---
+++ Part_2_Object_and_Classes/1_classes.js
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+// Welcome to the 1st exercise sheet of Programming Fundamentals in JavaScript!
+///////////////////////////////////////////////////////////////////////////////
+
+// Part 2 Object Oriented Programming.
+
+// EXERCISE 1. Classes (Old syntax).
+///////////////////////
+
+function Person(first, last, year) {
+    this.first = first;
+    this.last = last;
+    this.year = year;
+}
+
+var brendan = new Person('Brendan', 'Eich', 1961);
+
+Person.prototype.sayHi = function() {
+    console.log('Hi, I am ' + this.first);
+};
+
+brendan.sayHi();
+
+// EXERCISE 1. Classes (New syntax).
+///////////////////////////////////
+
+class Person {
+
+    constructor(first, last, year) {
+        this.first = first;
+        this.last = last;
+        this.year = year;
+    }
+
+    sayHi() {
+        console.log('Hi, I am ' + this.first);
+    }
+}
+
+var brendan = new Person('Brendan', 'Eich', 1961);
+
+// Behind there is still a prototype object.
+// So if you make changes to
+console.log(Person.prototype)
+console.log(brendan.__proto__)
---
+++ Part_2_Object_and_Classes/5_final_exercise.js
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// Welcome to the 1st exercise sheet of Programming Fundamentals in JavaScript!
+///////////////////////////////////////////////////////////////////////////////
+
+// Part 2 Object Oriented Programming.
+
+// EXERCISE 1. Classes.
+///////////////////////////////
+
+// Not really an exercise, it is more a small warm up to recall the different
+// primitive types in JavaScript and to get you familiar with ATOM Hydrogen.
