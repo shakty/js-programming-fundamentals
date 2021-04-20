1f991850cf64e88883ea5157efab5c9de4569ab4
Stefano Balieti <futur.dorko@gmail.com>
2021-04-20 14:05:47 +0200

    npm

--- Part_3_NPM/1_intro.js
+++ Part_3_NPM/1_intro.js
@@ -12,12 +12,17 @@
 // b. Make the following lines of code executables without 
 // errors.
 
+// Add something here.
+// Solution.
 const isOdd = require('is-odd');
 let odd  = 1;
 console.log(isOdd(odd));
 
 
 // c. Now, make these lines work...
+
+// Add something here.
+// Solution.
 const isEven = require('is-even');
 let even = 2;
 console.log(isEven(even));
@@ -40,14 +45,14 @@ console.log(even % 2 === 0);
 // EXERCISE 3. Understanding the packaging.
 ///////////////////////////////////////////
 
-// Did you notice that the 'is-odd' package has more than twice the downloads
+// a. Did you notice that the 'is-odd' package has more than twice the downloads
 // the package 'is-even'? (Right-column on the NPM web site, "Weekly Downloads")
 // Are developers more insecure when it comes to odd numbers? There is actually 
 // a more rational explanation. Check the source code of the packages inside the node_modules/ folder.
 
-// Hint: each nodeJS package contains a package.json file. This file contains
-// all the metadata about the project (name, author, keywords, etc.). The field // "main" contains the file that it is executed when you require it. Look 
-// inside the file referenced in "main" to answer the question.
+// Hint: each NodeJS package contains a package.json file. This file contains
+// all the metadata about the project (name, author, keywords, etc.); the field // "main" contains the name of the file that is executed by the require() 
+// function: check this file to answer the question.
 // To know everything about package.json files read here:
 // https://docs.npmjs.com/cli/v6/configuring-npm/package-json
 
@@ -61,6 +66,28 @@ console.log(even % 2 === 0);
 // fair, the package is-odd does a little bit more than the one-liner of 
 // Exercise 2. It also checks if the value is a valid integer. Still not worth
 // a dependency.
+
+// b. The the package isNumber is not part of the default node library. So, 
+// why does the following code work even if you never installed it? 
+// (because you didn't, right?). 
+
+const isNumber = require('is-number');
+console.log(isNumber("AhAh!"));
+console.log(isNumber(123));
+
+
+// Hint: Did you notice how the node_modules/ folder contains other packages // 
+// other than is-odd and is-even (such as is-buffer, is-number)? 
+// Who put them there? Check again the package.json files of is-odd, 
+// section "dependencies."
+
+
+// Solution.
+// The package "is-number" is a dependency of the package "is-odd." 
+// During installation, NPM flattens all dependencies of all packages inside 
+// the node_modules/ folder, becoming available to the NodeJS require() //
+// function.
+
 
 
 
