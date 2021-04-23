96aa29ba16009f63f0d8caee3b27c764cef24b4b
Stefano Balieti <futur.dorko@gmail.com>
2021-04-23 16:42:13 +0200

    fetch

--- Part_4_Async/5_fetch.js
+++ Part_4_Async/5_fetch.js
@@ -9,7 +9,7 @@ const fetch = require('node-fetch');
 
 const ENDPOINT = 'https://swapi.dev/api/';
 
-let query = 'people/1';
+let query = 'people/';
 
 // fetch(ENDPOINT + query)
 //   .then(res => console.log(res));
@@ -19,40 +19,104 @@ let query = 'people/1';
 // Why 400.
 // https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 
-fetch(ENDPOINT + query)
-  .then(res => {
-    if (res.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-    return res.json();
-  })
-  .then(user => {
-    console.log(user);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+// fetch(ENDPOINT + query)
+//   .then(res => {
+//     if (res.status >= 400) {
+//       throw new Error("Bad response from server");
+//     }
+//     return res.json();
+//   })
+//   .then(json => {
+//     console.log(json);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
 
 
-// Exercise 2. Async Fetch.
-///////////////////////////
+// // Exercise 2. Async Fetch.
+// ///////////////////////////
 
-// Await works only inside an async function.
-(async() => {
-  try {
-    const res = await fetch(ENDPOINT + query);
+// // Await works only inside an async function.
+// (async() => {
+//   try {
+//     const res = await fetch(ENDPOINT + query);
 
-    if (res.status >= 400) {
-      throw new Error("Bad response from server");
-    }
+//     if (res.status >= 400) {
+//       throw new Error("Bad response from server");
+//     }
 
-    const user = await res.json();
+//     const user = await res.json();
 
-    console.log(user);
+//     console.log(user);
 
-  } 
-  catch(err) {
-    console.error(err);
-  }
-})();
+//   } 
+//   catch(err) {
+//     console.error(err);
+//   }
+// })();
   
+
+// Exercise 3. Optional. Fetch them all.
+////////////////////////////////////////
+
+let db = [];
+let page = 1;
+
+let doFetch = (page = 1) => {
+  console.log('Fetching page ' + page);
+  fetch(ENDPOINT + query + '?page=' + page)
+    .then(res => {
+      if (res.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return res.json();
+    })
+    .then(json => {
+      db = [ ...db, ...json.results ];
+      doFetch(++page);
+    })
+    .catch(err => {
+      console.log(`Fetched ${db.length} Star Wars characters.`);
+      // console.error(err);
+      // fetching = false;
+    });
+}
+
+doFetch();
+
+
+
+
+// let fetchAll = async () => {
+//   let db = [];
+//   let fetching = true;
+//   let page = 1;
+//   try {
+//     while(fetching) {
+//       console.log('Fetching page ' + page);
+//       const res = await fetch(ENDPOINT + query + '?page=' + page++);
+      
+//       if (res.status >= 400) {
+//         throw new Error("Bad response from server");
+//       }
+
+//       const json = await res.json();
+
+//       db = [ ...db, ...json.results ];
+//     }
+//   } 
+//   catch(err) {
+//     // console.error(err);
+//     fetching = false;
+//   }
+//   return db;
+// };
+
+// (async() => {
+//   let db = await fetchAll();
+//   console.log(`Fetched ${db.length} Star Wars characters.`);
+// })();
+
+
+
