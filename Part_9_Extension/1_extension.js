c4915943a1ce5ad514ae8a7cc2242b72a115ad4f
Stefano Balieti <futur.dorko@gmail.com>
2021-04-28 16:52:09 +0200

    extension

---
+++ Part_9_Extension/1_extension.js
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////
+// Programming Fundamentals in JavaScript! //
+/////////////////////////////////////////////
+
+// Module: Chrome Extension.
+////////////////////////////
+
+// Chrome extensions _extend_ the functionalities of the 
+// Chrome browser.
+
+// https://developer.chrome.com/docs/extensions/
+
+// Every major browser is somewhat extendible and the language to do
+// is JavaScript-based, however there some differences. We will focus
+// only on Chrome extensions. 
+
+// A web server is a computer software that replies to incoming
+// network requests by serving files (hmtl pages, images, videos, JSON, ...).
+
+// The philosophy of Express is to be minimal and to allow for 
+// complex configurations by the programmers.
+
+// Exercise 1: Create your first Express server.
+////////////////////////////////////////////////
+
+// Type in the console:
+
+// npm install express
+
+// When ready, start the server and point the browser to:
+
+// http://localhost:3000/
+
+// to check that it is working.
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Start the server on port 3000.
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+// As I said, Express is **mininal**. Or lazy. It does not do
+// anything unless you tell him so. So let's add some routes.
+
+// Routes match a path from the root address to a callback function.
+// The callback takes (at least) two input parameters:
+// 
+// - req: an object with info about the incoming network request,
+// - res: an object with info and methods to respond to the request.
+//
+// (more details on req and res in the next exercises).
+
+// Intercepts all requests.
+app.get('/', (req, res) => {
+    res.send('Yay! The server is running!');
+});
+
+// Intercepts requests through the path /secret.
+app.get('/secret', (req, res) => {
+    res.send('How did you know about this route? It was a secret!');
+});
+
+// Checkpoint.
+// 1. What do you see?
+// 2. What do you see at route: http://localhost:3000/secret/ ?
+// 3. What do you see at route: http://localhost:3000/SECRET/ ?
+// 4. What do you see at route: http://localhost:3000/notexisting/ ?
