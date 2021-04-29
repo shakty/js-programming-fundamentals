9df6997a46eeaa562020e9d849038dd794db919d
Stefano Balieti <futur.dorko@gmail.com>
2021-04-29 22:55:30 +0200

    more ext

--- 9_Chrome_Extensions/extensions/fblock/background.js
+++ 9_Chrome_Extensions/extensions/fblock/background.js
@@ -1,23 +1,29 @@
 // Whenever there is an update on any tab in the browser
 // this function is called.
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  
+
   // We loaded a new tab.
   if (changeInfo.status == 'complete' && tab.active) {
 
-    // See data if you are curious.
+    // Look into the tab data if you are curious.
     // console.log(tabId);
     // console.log(tab);
 
-    // If we are on Facebook let's do something.
+    // If the url contains "facebook", let's do something.
     if (tab.url.indexOf('facebook') !== -1) {
 
+      // Somehow, a delay is needed to prevent alert messages
+      // to be removed too quickly. 
       setTimeout(function() {
+
+        // We cannot write JS code directly here, we need to
+        // inject it into the page with the scripting API.
+        // https://developer.chrome.com/docs/extensions/reference/scripting/
         chrome.scripting.executeScript({
-          target: {tabId: tabId, allFrames: true },
+          target: { tabId: tabId, allFrames: true },
           files: [ 'content-script.js' ]
         });
-        
+
       }, 1000); 
 
     }
--- 9_Chrome_Extensions/extensions/fblock/content-script.js
+++ 9_Chrome_Extensions/extensions/fblock/content-script.js
@@ -1,91 +1,81 @@
-alert('Again on Fb!');
+alert("You are on Facebook! Do not panic, we got you covered.");
 
+// How Facebook writes "Sponsored". Why?
+const SPONSORED = "S\nt\nS\nS\np\ns\no\np\no\nn\nh\ns\no\nf\nt\nt\nn\nr\nS\nso\nr\nf\nr\ne\ng\nd\ned\nr";
 
-let SPONSORED = "S\nt\nS\nS\np\ns\no\np\no\nn\nh\ns\no\nf\nt\nt\nn\nr\nS\nso\nr\nf\nr\ne\ng\nd\ned\nr";
+const SPONSORED2 = "S\nr\npo\nt\nh\nl\ng\nu\nn\nS\nS\np\no\no\nn\nu\ns\no\na\nn\nr\ne\ns\nm\nored\nd"
 
-setInterval(doSearch, 5000);
+let lastSpanFound = 0;
+
+let hiddenPosts = [];
+
+// We do the search for unwanted posts every 5 seconds, not great.
+let interval = setInterval(doSearch, 5000);
 
 function doSearch() {
-    let classes = "b6zbclly myohyog2 l9j0dhe7 aenfhxwr l94mrbxd ihxqhq3m nc684nl6 t5a262vz sdhka5h4"; 
-    let sponsored = document.getElementsByClassName(classes);
+
+    // It's very difficult to locate via attributes 
+    // the sponsored posts, so we look into all <SPAN> tags. 
     let spans = document.getElementsByTagName('span');
     
-
-    console.log('HERE!!', spans.length);
-
-    for (let i = 0; i < spans.length ; i++) {
+    console.log('<SPAN>s found', spans.length);
+    // Facebook keeps add new spans as we scroll down, 
+    // no need to re-search the same one over and over,
+    // so we start from the last examined. 
+    for (let i = lastSpanFound; i < spans.length ; i++) {
+        
+        // If you are curious to look into all texts.
         // console.log()
         // console.log()
         // console.log(spans[i].innerText);
         // console.log()
         // console.log()
-        if (spans[i].innerText === SPONSORED) {
-            spans[i].innerHTML = 'AAAAAAAAAAAA! ' + i;
-            findPageLet(spans[i]);
-            // spans[i].click();
+        
+        if (spans[i].innerText === SPONSORED ||
+            spans[i].innerText === SPONSORED2) {
+            let res = hidePost(spans[i]);
+            // If post was hidden, add a reference to the hidden posts.
+            if (res) hiddenPosts.push(i);
         }
     }
 
-    // let links = document.getElementsByTagName('a');
-    // console.log('HERE!!', links.length);
-
-
-    // for (let i = 0; i < links.length ; i++) {
-    //     // console.log(links[i].innerHTML);
-    //     if (links[i].href.indexOf('utm_campaign') !== -1 ||
-    //         links[i].href.indexOf('fbclid') !== -1
-    //        ) {
-    //         links[i].style.display = 'none';
-    //         console.log('Found: ', i);
-    //         debugger
-    //         let counter = 0;
-    //         let found = false;
-    //         let failsafe = 100;
-    //         let curElement = links[i];
-    //         while (counter < failsafe && !found) {
-    //             let parent = curElement.parentNode;
-    //             if (!parent) break;
-    //             if (parent.dataset && parent.dataset.pageLet) {
-    //                 parent.style.display = "none";
-    //                 found = true;
-    //             }
-    //             else {
-    //                 curElement = parent;
-    //                 counter++;
-    //             }
-    //         }
-    //         if (found) console.log('YEAH!!!');
-    //         else ('DOH!')
-    //         console.log();
-    //         // spans[i].click();
-    //     }
-    // }
-
-
-
-    // debugger
-
-    // for (let i = 0; i < sponsored.length ; i++) {
-    //     console.log(sponsored[i].innerHTML);
-    //     if (sponsored[i].innerHTML === 'Sponsored') {
-    //         sponsored[i].innerHTML = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAA!';
-    //     }
-    // }
-
+    // Keep reference to the last span found.
+    lastSpanFound = spans.length;
 }
 
 
-function findPageLet (el) {
+// function isSponsored(el) {
+//     let children = el.children;
+//     let idx = 0;
+//     let sponsored = 'Sponsored';
+//     debugger;
+//     for (let i = 0 ; i < children.length ; i++) {
+//         // "position: absolute; top: 3em;"
+//         if (!children[i].hasAttribute('style')) {
+//             if (children[i].innerText !== sponsored.charAt(idx)) return false;
+//             idx++;
+//         }
+//     }
+// }
+
+function hidePost (el) {
+        
+    // We go up the DOM, from child to parent and
+    // hide the whole post (8 parents above is enough).
+    const LIMIT = 9; 
     let counter = 0;
     let curElement = el;
-    const LIMIT = 9; // 8 parents above is enough
     while (counter < LIMIT) {
         let parent = curElement.parentNode;
         if (!parent) break;
+        parent.style.display = 'none';
         parent.innerHTML = '';
         curElement = parent;
         counter++;
     }
-    console.log();
-    // spans[i].click();
+    if (parent) {
+        parent.style.display = 'none';
+        return true;
+    }
+    return false;
 }
